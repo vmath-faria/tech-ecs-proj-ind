478b75ef79d20b3843434cdff3709fdd6f11e5db
vmath-faria <victor.faria@sptech.school>
2025-11-25 16:19:05 -0300

    terminei o projeto

--- web-data-viz/src/database/script-tabelas-techecs.sql
+++ web-data-viz/src/database/script-tabelas-techecs.sql
@@ -23,44 +23,15 @@ yrRelease int,
 titulo varchar(255),
 descricao varchar(255),
 vocalista varchar(255));
-insert into usuario (username, email, senha, fkAlbum, qtAcertadas) values(
-"victor2006", "victor2006@email.com", "victor#2006", 4, 6),
-("luana2007", 'luana2007@email.com', 'luana#2007', 8, 4),
-("isabela2005", 'isabela2005@email.com', 'isabela#2005', 3, 6);
 insert into questao values
 (1, "Qual integrante da banda nunca saiu?"),
 (2, "Quantos álbuns a banda lançou?"),
 (3, 'Álbum Mais Vendido:'),
 (4, 'Quantos cantores gravaram álbuns pela banda?'),
 (5, "Ano de lançamento do álbum 'Headless Cross':"),
-(6, 'Que cover o BLACK SABBATH durante a turnê desse álbum?'),
+(6, 'Que cover o BLACK SABBATH fez durante a turnê desse álbum?'),
 (7, 'Qual foi o último álbum com Ozzy Osbourne antes de sua 1ª saída?'),
 (8, 'Que instrumento cada integrante toca?');
-insert into resultadoquiz (fkUsuario, acertou, fkQuestao)values(
-1, 1, 1),
-(1, 0, 2),
-(1, 1, 3),
-(1, 0, 4),
-(1, 1, 5),
-(1, 1, 6),
-(1, 1, 7),
-(1, 1, 8),
-(2, 1, 1),
-(2, 1, 2),
-(2, 1, 3),
-(2, 0, 4),
-(2, 1, 5),
-(2, 0, 6),
-(2, 0, 7),
-(2, 0, 8),
-(3, 1, 1),
-(3, 0, 2),
-(3, 1, 3),
-(3, 0, 4),
-(3, 1, 5),
-(3, 1, 6),
-(3, 1, 7),
-(3, 1, 8);
 insert into album (idAlbum, titulo, descricao, vocalista, yrRelease) values
 	(1, 'Black Sabbath', 'Primeiro álbum da banda', 'Ozzy Osbourne', 1970),
 		(2, 'Paranoid', 'Álbum mais famoso', 'Ozzy Osbourne', 1970),
@@ -84,10 +55,10 @@ insert into album (idAlbum, titulo, descricao, vocalista, yrRelease) values
 																				(20, '13', 'Último álbum do BLACK SABBATH', 'Ozzy Osbourne', 2013);
 select * from resultadoquiz join usuario on fkUsuario=idUsuario
 	join album on fkAlbum=idAlbum join questao on fkQuestao=idQuestao;
-select (select count(qtAcertadas) from usuario where qtAcertadas<=3) as "Am I Going Insane",
-	(select count(qtAcertadas) from usuario where qtAcertadas>3 and qtAcertadas<=6) as "Neon Knight",
-		(select count(qtAcertadas) from usuario where qtAcertadas>=7) as "Supernaut" from usuario;
-select q.idQuestao, q.descricao from questao as q
+select (select count(qtAcertadas) from usuario where qtAcertadas<=3) as "AIGI",
+	(select count(qtAcertadas) from usuario where qtAcertadas>3 and qtAcertadas<=6) as "NK",
+		(select count(qtAcertadas) from usuario where qtAcertadas>=7) as "SN" from usuario;
+select q.descricao from questao as MQ
 	join resultadoquiz r on r.fkQuestao = q.idQuestao
 		group by q.idQuestao, q.descricao order by SUM(r.acertou)
 			desc limit 1;
